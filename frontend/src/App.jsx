1ae866d8105c51b86025fb02ca657270671290ad
KrishMaheshwariDev <krishmaheshwari197903@gmail.com>
2026-08-14 17:16:13 +0530

    HLD complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import FindMyDocument from './components/FindMyDocument'
 
 function App() {
-  
+  return (
+  );
 }
 
 export default App
